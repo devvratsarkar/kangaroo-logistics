c709c0884e7eecd0d6a9228317290c7a651cdc84
Devvratbytelogic <support@bytelogicindia.com>
2026-08-26 20:59:36 +0530

    add scroll restoration to main layout

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, ScrollRestoration } from 'react-router-dom'
 import PrimaryFooter from './footer/PrimaryFooter.jsx'
 import PrimaryHeader from './header/PrimaryHeader.jsx'
 
 export default function MainLayout() {
   return (
     <div className="min-h-svh bg-white text-primary">
+      <ScrollRestoration />
       <PrimaryHeader />
       <main>
         <Outlet />
